9ede9706074e7f346c84a07b591da7f5e2fc52d8
rafi4482 <rafi.kazla@gmail.com>
2021-09-02 19:41:49 +0600

    fbbbb

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ if(searchText===""){
 }
 error1.innerText=''
 searchField.value=''
-const url=`http://openlibrary.org/search.json?q=${searchText}`
+const url=`https://openlibrary.org/search.json?q=${searchText}`
 fetch(url)
 .then(res=>res.json())
 .then(data=>display(data))
