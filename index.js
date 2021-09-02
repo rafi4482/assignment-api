6b49fc23f5d7ba2245a43ed4fde8055496cd5177
rafi4482 <rafi.kazla@gmail.com>
2021-09-02 14:56:08 +0600

    firs

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const error1= document.getElementById('error-message')
 
+// taking input
 const searchBook = ()=>{
-
 const searchField=document.getElementById('search-field')
 const searchText=searchField.value
 if(searchText===""){
-  error1.innerText='empty'
+  error1.innerText='search field cannot be empty'
   return
 }
 error1.innerText=''
@@ -14,24 +14,28 @@ const url=`http://openlibrary.org/search.json?q=${searchText}`
 fetch(url)
 .then(res=>res.json())
 .then(data=>display(data.docs))
-// .catch(error=>dg(error))
 }
-
+// display data
 const display = doc =>{
 const searchResult = document.getElementById('search-result')
 searchResult.innerHTML=''
+if(doc.length===0){
+  error1.innerText="nothing found"
+}
 doc.forEach(docu=>{
 const div = document.createElement('div')
 const bn = document.getElementById('booknumber')
 bn.innerText=`Total Results Found:${doc.length}`;
+bn.innerText=''
 div.classList.add('col')
 div.innerHTML =`
 <div class="card h-100">
         <img src="https://covers.openlibrary.org/b/id/${docu.cover_i}-L.jpg" class="card-img-top" alt="...">
         <div class="card-body">
-          <h5 class="card-title">Book Author:${docu.author_name} Book Title:${docu.title}</h5>
-          <p class="card-text">Book Publisher:${docu.publisher}</p>
-          <p> First Publish Year:${docu.first_publish_year}</p>
+          <h5 class="card-title">${docu.title}</h5>
+          <p><b>Book Author:</b>${docu.author_name}</p>
+          <p><b>Book Publisher:</b>${docu.publisher}</p>
+          <p><b>First Publish Year:</b>${docu.first_publish_year}</p>
           </div>
       </div>
 
